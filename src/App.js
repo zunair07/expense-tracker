4d9c75a04386081759c28f8c454338a76c221d74
zunair07 <ahmadzunair21@gmail.com>
2020-06-14 01:48:21 +0500

    Revert "Expense_Tracker_App"
    This reverts commit fc2249e698f548cca8f044eddee258eff9123c5c.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import Header from './components/Header';
-import Balance from './components/Balance';
-import IncomeExp from './components/IncomeExp';
-import Transactionlist from './components/Transactionlist';
-import Addtransection from './components/Addtransection';
-import { GlobalProvider } from './context/GlobalState';
+import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
-    <GlobalProvider>
-
-      <Header />
-     
-     <div className="container">
-      <Balance />
-      
-      <IncomeExp />
-      <Transactionlist />
-      <Addtransection />
-      </div>
-
-      </GlobalProvider>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
